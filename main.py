988aa937809846eb60041d81548712ee91ff0718
sekpona1 <sekpona31@gmail.com>
2021-06-23 15:35:47 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+
+import matplotlib.pyplot as plt
+from fastapi import FastAPI, UploadFile, File
+import pytesseract as tess
+from PIL import Image
+import numpy as np
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+import re
+from gensim.summarization import summarize
+from PIL import Image
+from io import BytesIO
+import pytesseract as tess
+tess.pytesseract.tesseract_cmd = r'C:\Users\sitsope sekpona\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+from PIL import Image
+
+def tesser(image):
+    img = Image.open(BytesIO(image))
+    text = tess.image_to_string(img)
+    return {"text": text}
+
+
+#Creation d'une api
+app=FastAPI()
+@app.post('/ocr_pred/')
+async def ocr1(file: bytes = File(...)):
+    #image = Image.open(image)
+    prediction = tesser(file)
+    return prediction
+
